cf6923b2bcddd302d1c6d9e740f5f944db9b49dd
zanderp25 <zander@zanderp25.com>
2025-02-21 20:23:00 -0500

    Add PM2 environment file

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps : [{
+        name   : "CTRL-ALT-DEFEAT",
+        script : "/usr/local/bin/yarn",
+        args   : "prod",
+        cwd   : "/home/zanderp25/CTRL-ALT-DEFEAT",
+    }]
+}
